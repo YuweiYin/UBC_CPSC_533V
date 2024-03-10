d0b34e2d277f3a43855404fbbd8251209dcec2ad
YuweiYin <seckexyin@gmail.com>
2024-03-09 22:43:15 -0800

    UBC_CPSC_533V

--- Project/get_offline_data.sh
+++ Project/get_offline_data.sh
@@ -9,16 +9,16 @@ conda activate 533v
 python3 get_offline_data.py --task "download" > "${LOG_DIR}/download_all.log"
 
 parse_nohup(){
-  data=$1
+  env=$1
   level=$2
-  echo -e ">>> Parse: ${data}-${level} <<<"
-  nohup python3 get_offline_data.py --task "parse" --data "${data}" --level "${level}" > \
-    "${LOG_DIR}/${data}-${level}.log" 2>&1 &
+  echo -e ">>> Parse: ${env}-${level} <<<"
+  nohup python3 get_offline_data.py --task "parse" --env "${env}" --level "${level}" > \
+    "${LOG_DIR}/${env}-${level}.log" 2>&1 &
 }
 
-for cur_data in "halfcheetah" "hopper" "walker2d" "ant"; do
+for cur_env in "halfcheetah" "hopper" "walker2d" "ant"; do
   for cur_level in "random" "medium" "expert" "medium-replay" "medium-expert"; do
-    parse_nohup "${cur_data}" "${cur_level}"
+    parse_nohup "${cur_env}" "${cur_level}"
   done
 done
 
